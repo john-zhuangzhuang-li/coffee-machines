7a8f91c45fb91677eac9adb0f306425b0e7a1c13
john-zhuangzhuang-li <john-zhuangzhuang-li@outlook.com>
2022-10-15 02:58:38 -0400

    Added env local

--- util/firebase.ts
+++ util/firebase.ts
@@ -1,8 +1,18 @@
 import { initializeApp } from "firebase/app";
-import { firebaseConfig } from "./firebase.config";
+// import { firebaseConfig } from "./firebase.config";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
